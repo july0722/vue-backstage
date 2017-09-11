9fb843c92c3937881826dc4c2b6c27036f2f432d
july0722 <julyzwj@yeah.net>
2017-09-11 14:30:36 +0800

    update regex

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,7 +1,4 @@
-export function validateMobilePhone(str) {
-  return /^1(?:3[0-9]|5[012356789]|8[0-9]|7[0-9])\d{8}$/.test(str)
-}
 
-export function validateEmail(str) {
-  return /(^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$)/.test(str)
-}
+export const mobile = /^1(?:3[0-9]|5[012356789]|8[0-9]|7[0-9])\d{8}$/
+
+export const email = /(^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$)/
